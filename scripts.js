f23d7aa87b5c9b91b4c8df8ac439d2790d92f5df
Nour <186416386+Noureddin94@users.noreply.github.com>
2026-09-11 17:49:40 +0200

    Signup done

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
-// document.getElementsByClassName("email-signup")[0].style.display = "none";
-// document.getElementById("signup-box-link").style.display = "none";
-// document.getElementById("signup-box-link").addEventListener("click", function(){
-//   document.getElementsByClassName("email-login")[0].style.display = "none";
-//   document.getElementsByClassName("email-signup")[0].style.display = "block";
-//   document.getElementById("login-box-link").classList.remove("active");
-//   document.getElementById("signup-box-link").classList.add("active");
-// });
+document.getElementsByClassName("email-signup")[0].style.display = "none";
+document.getElementById("signup-box-link").style.display = "none";
+document.getElementById("signup-box-link").addEventListener("click", function(){
+  document.getElementsByClassName("email-login")[0].style.display = "none";
+  document.getElementsByClassName("email-signup")[0].style.display = "block";
+  document.getElementById("login-box-link").classList.remove("active");
+  document.getElementById("signup-box-link").classList.add("active");
+});
 document.getElementById("login-box-link").addEventListener("click", function(){
   document.getElementsByClassName("email-login")[0].style.display = "block";
-//   document.getElementsByClassName("email-signup")[0].style.display = "none";
+  document.getElementsByClassName("email-signup")[0].style.display = "none";
 
   document.getElementById("login-box-link").classList.add("active");
-//   document.getElementById("signup-box-link").classList.remove("active");
+  document.getElementById("signup-box-link").classList.remove("active");
 });
